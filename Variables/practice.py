45271af8e90c04cec15ef8d6400534b8fc39350e
What-crt <mykel.allan@ucas-edu.net>
2025-09-03 19:25:00 +0000

    ms larose

---
+++ Variables/practice.py
@@ -0,0 +1,23 @@
+# 6th Github connection
+
+your_name = "Mykel"
+number_1_10 = 5
+number_100_1000 = 705
+breakfast = "cereal" 
+favorite_color = "blue"
+school_name = "UCAS"
+year = 2025
+eye_color = "hazel"
+age = 14
+favorite_subject = "Math"
+
+print("your name:", your_name)
+print("A number between 1 and 10:", number_1_10)
+print("A number between 100 and 1000:", number_100_1000)
+print("What you had for breakfeast:", breakfast)
+print("Your favorite color:", favorite_color)
+print ("The school name:", school_name)
+print("The year:", year)
+print("Your eye color:", eye_color)
+print("Your age:", age)
+print("Your favorite subject in school:", favorite_subject)

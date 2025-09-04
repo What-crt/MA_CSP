1879baf6649a2970256d220d4fc2f28080e83605
What-crt <mykel.allan@ucas-edu.net>
2025-09-04 13:43:09 -0600

    wrote expression notes for CSP

---
+++ practices/expressions.py
@@ -0,0 +1,42 @@
+# MA 6th expression notes
+
+# 1. what is an What is an algorithm?
+    # An algroith is a set of instructions to acomplish a task. - Sarah had 10 apples to give to 6 friends. How many apples does each person get?--> 10/6 = 1.66--> friend most likely gets 1. a collection of numbers and symbols to get a result.
+name = input("What is your name? ")
+print("Hello", name)
+
+length = 5
+width = 20
+area = length * width 
+print(length, "*", width, "=", area)
+
+# 2. List steps in an algorithm
+    # In the computer- step 1. determine variables we need. what do we need? step 2. process information. step 3. provide output. 
+age1 = 56
+age2 = 15
+age3 = 3
+age4 = 34
+number_of_people_in_group = 4
+total = age1 + age2 + age3 + age4 
+average = total/4
+print("The average age of four people who are 56, 3, 15, and 34 is-", average)
+
+num_one = 12
+num_two = 4
+print("math equations with 12 and 4;")
+print("addition-", num_one + num_two)
+print("subtraction-", num_one - num_two)
+print("multiplication-", num_one * num_two)
+print("division-", num_one/num_two)
+print("exponents-", num_one ** num_two)
+print("Integer divison-", num_one//num_two)
+print("modulo (remainder)-", num_one % num_two)
+
+# 3.List ALL of the different mathematical operators (Give me the symbol and tell me what it does)
+    #
+
+# 4. List ALL of the different assignment operators (Give me the symbols and what it does)
+    #
+
+# 5. Why are expressions so important in programming?
+    #

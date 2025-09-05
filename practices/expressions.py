b9330d0d79c7aefc5eaf07be3c2c8d1f24faaa33
What-crt <mykel.allan@ucas-edu.net>
2025-09-05 13:26:08 -0600

    finish expression notes

--- practices/expressions.py
+++ practices/expressions.py
@@ -9,6 +9,7 @@ length = 5
 width = 20
 area = length * width 
 print(length, "*", width, "=", area)
+print("")
 
 # 2. List steps in an algorithm
     # In the computer- step 1. determine variables we need. what do we need? step 2. process information. step 3. provide output. 
@@ -20,7 +21,10 @@ number_of_people_in_group = 4
 total = age1 + age2 + age3 + age4 
 average = total/4
 print("The average age of four people who are 56, 3, 15, and 34 is-", average)
+print("")
 
+# 3.List ALL of the different mathematical operators (Give me the symbol and tell me what it does)
+    # *= multiplacation /= divison += addition -= subtraction **= exponants //=Interger division %= modulo (remainder)
 num_one = 12
 num_two = 4
 print("math equations with 12 and 4;")
@@ -31,12 +35,32 @@ print("division-", num_one/num_two)
 print("exponents-", num_one ** num_two)
 print("Integer divison-", num_one//num_two)
 print("modulo (remainder)-", num_one % num_two)
-
-# 3.List ALL of the different mathematical operators (Give me the symbol and tell me what it does)
-    #
+#float- a decimal 
+#inteager- a whole number
 
 # 4. List ALL of the different assignment operators (Give me the symbols and what it does)
-    #
+    #this is how you assign each promblem a variable basically. 
+num_one = 12
+num_two = 4
+print("")
+print("math equations with 12 and 4;")
+num_one += num_two
+print("addition-", num_one)
+num_one -= num_two
+print("subtraction-", num_one)
+num_one *= num_two
+print("multiplication-", num_one)
+num_one /= num_two
+print("division-", num_one)
+num_one **= num_two
+print("exponents-", num_one)
+num_one //= num_two
+print("Integer divison-", num_one)
+num_one %= num_two
+print("modulo (remainder)-", num_one)
+print("")
 
 # 5. Why are expressions so important in programming?
-    #
+    #Because we are less efficient than computers. Because if you set it up once you don't ever have to set it again. 
+
+

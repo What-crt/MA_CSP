94f67d2f362f93720c27a5215e707e8ab337c31e
What-crt <mykel.allan@ucas-edu.net>
2025-09-08 13:45:14 -0600

    finish string notes

--- practices/String_notes.py
+++ practices/String_notes.py
@@ -1,3 +1,34 @@
 # MA 6th string notes
 
 print("I did it!")
+
+#1.What makes something a string?
+#a collection of symbols held together by quotation marks, numbers and letters. 
+"content of string"
+'this is also a string'
+#quotation marks on any side have to match. every input has a string assosiated with it. 
+#EXAMPLES:
+first_name = input("what is your first name?").strip().title()
+
+Last_name = input("what is your last name?").strip().title()
+
+full_name = first_name + " " + Last_name
+
+sentence = "the quick brown fox jumps over the lazy dog."
+
+print("welcome to my program", full_name +"!")
+
+#2.Why do we have strings?
+
+#3.How do stupid proofing and sanitization relate to each other?
+
+#4.What is debugging?
+
+#5.How do you debug the different types of errors?
+
+#6.Describe what each of the methods below does:
+#A. find()
+#B. concatenate (add)
+#C. upper()
+#D. lower()
+#E. strip()

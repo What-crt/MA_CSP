25610ea409d8df9447c00d07029620f004e94f0e
What-crt <mykel.allan@ucas-edu.net>
2025-09-10 13:42:59 -0600

    work on silly sentences

---
+++ practices/silly_sentences.py
@@ -0,0 +1,13 @@
+# MA 6th silly sentences
+Adjective =  input("Give a ajective-")
+realitives_Name = input("Give a relitives name-")
+noun_1 = input("Give a noun-")
+famous_person = input ("Give a famous persons name-")
+animal = input ("Give a animal-")
+piece_of_clothing = input ("Give a piece of clothing-")
+place = input ("Give a place-")
+band = input("Give a name of a band-")
+Title_of_song = input ("Give a made up title of a song-")
+noun_2 = input ("Give a noun-")
+
+print("Yesterday was", Adjective, "I woke up to the noise of",  realitives_Name, "banging on my", noun_1 + ". When I went to school I saw", famous_person, "walking down the street, Walking a", animal + ". I thought I was imagining it. When I got home my mom told me to put on my", piece_of_clothing, "on so we could go to the", place + ". When we got to see", band, "preform their new song '" + Title_of_song + ".' That night I fell fast asleep on my cozy", noun_2 + ".")

65e64fe2bdcf60f976c4ceb1499c15b855e28c8c
What-crt <mykel.allan@ucas-edu.net>
2025-09-16 13:27:25 -0600

    work on update hello

---
+++ practices/update_hello.py
@@ -0,0 +1,10 @@
+# MA 6th update hello world
+def hello(): 
+    name = input("What is your name?")
+    print(f"Hello {name}!")
+                                
+hello()
+hello()
+hello()
+hello()
+hello()

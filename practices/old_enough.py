f0812b119c8f31800b388dfd25b675550b154849
What-crt <mykel.allan@ucas-edu.net>
2025-09-18 13:43:32 -0600

    finished old enough assignment :) :)

--- practices/old_enough.py
+++ practices/old_enough.py
@@ -1,10 +1,13 @@
-# MA 6th old enough   
+# MA 6th old enough  
 
 age = int(input("What is your age?"))
 
-if age < 18:
-    print("You are a minor.")
-elif age >= 18 and age < 65:
-    print("You are an adult.")
+if age >= 18:
+    print("You are old enough to vote!")
+elif age >= 16:
+    print("You are old enough to drive!")
+elif age == 15:
+    print("You are old enough to get a learners permit!")
 else:
-    print("You are a senior citsen!")
+    if age >= 4:
+        print("You are old enough to go to school!")

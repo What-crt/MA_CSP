031efd84f951e7689517f66f95273e8dfcc61b31
What-crt <mykel.allan@ucas-edu.net>
2025-09-22 13:42:09 -0600

    work on for loops notes

--- practices/notes/for_loops.py
+++ practices/notes/for_loops.py
@@ -1,36 +1,77 @@
 # MA 6th loop notes
 
 #What is a loop? 
+    #keep going until a specific condition is met.
 
 import datetime
-
 current = datetime.datetime.now()
 hour = current.hour
 
 print(f"The time is: {current}")
 print(f"hour is: {hour}")
 
+siblings = ["Tyler", "Kenyan", "Mykel", "Jack"]
+
+print(siblings[2])
+print(siblings)
+siblings[0] = "Tc"
+siblings.remove("Mykel")
+
+    #For loops
+for x in siblings:
+    #print(f"Hello {x}")
+    print("Hi!")
+
+for x in range(1,6):
+    print(x)
+for x in range(20, -11, -1):
+    print(x)
+    # 20  start, -11 end, -1 count by.
+
 
 #What are the two types of loops?
+    #for loops and while loops. 
+#while True:   #infinite loop
+    #print("oh No!")    #infinte loops are bad!!!!!
+    #1 iterator(kepp track of loop #)
+    #2 end condition(tells the loop to stop)
+    #3 increase the iterator
+#good while loop
+i = 1
+
+while i < 21:
+    if i % 2 == 0:
+        print(f"{i} is even.")
+    else:
+        print(f"{i} is odd.")
+    i += 1
+
+import random
+number = random.randint(1,20)
+x = 1
+#while x != number:
+   # print("Duck")
+    #x += 1
+#print("Goose!")
+
+while True:
+    if number == x:
+        print("Goose!")
+        break  #breaks the loop 
+    else:
+        print("Duck")
+        x += 1
+
 #What is iteration
+    #
+
 #What are lists? 
     #complex data type. store multiple pieces of information in the same variable.
 siblings = ["Tyler", "Kenyan", "Mykel", "Jack", ["Glenn", "Sam", "Eliza"], 5, 3.14]
 
 #How do you make lists in python? 
+    #
 #How do you make for loops in python? 
+    #
 #How do you make while loops in python?
-
-
-
-
-import time
-
-epoch = time.time()
-current = time.ctime(epoch)
-local_time = time.localtime(current)
-hour = local_time.tm_hour
-
-print(f"the time in secounds since Jan 1, 1970: {epoch}")
-print(f"The time is: {current}")
-print(f"hour is: {hour}")
+    #

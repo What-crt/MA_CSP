6200740a40764e3dbb6c12e0410c87ba5b44f552
What-crt <mykel.allan@ucas-edu.net>
2025-10-06 13:43:55 -0600

    EX

---
+++ c_programming/notes/expresion_notes.c
@@ -0,0 +1,53 @@
+//# MA expressions notes for c
+#include <stdio.h>
+#include <math.h>
+
+int main (void){
+    int year = 2025;//Whole decimals
+    float pi = 3.14;//decimals
+    double long_pi = 3.14159265359;// decimals that are 2x as long
+
+    printf("8/3 = %d\n", (float) 8/3); //cast is specifically stating the data type
+    printf("8/3 = %1.2f\n", 8/3.0);
+    printf("2 ^ 4 = %f\n", pow(2, 4));
+
+    year += 1;
+    year ++;
+
+    return 0;
+}
+//What is the difference between an integer and a float in C?
+    //integers are whole  numbers while floats are decimals.
+
+//How does C handle integer division compared to float division?
+    //It handles the same but intergers always come out as a whole number and floats can be decimals.
+
+//List the arithmetic operators available in C and their functions.
+    //
+
+//What is the modulus operator, and how is it used?
+    //(%) It calculates the reminder of an integer division.
+
+//How do you round a float to the nearest integer in C?
+    //You use <math.h> to get roundf(). rounds value to the nearest integer.
+
+//What is type casting in C? Provide an example of explicit type casting.
+    //
+
+//How do compound assignment operators work in C? List three examples.
+    //
+    /* addition
+        int x = 5;
+    x += 3;
+        multiplication
+        int x = 5;
+    x += 3; 
+    */
+//What is the purpose of the math.h library? Name three functions it provides.
+
+
+//How do you print a float with a specific number of decimal places using printf()?
+
+
+//What happens when you mix integer and float operations in C?
+

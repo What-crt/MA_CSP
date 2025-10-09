a2937b31bd66da5bda6e976db047396b7c5a8945
What-crt <mykel.allan@ucas-edu.net>
2025-10-09 13:42:45 -0600

    work on string notes and name decorator for c

---
+++ c_programming/notes/string_notes.c
@@ -0,0 +1,38 @@
+// MA 6th string notes for c
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+
+
+    char name[100]= "Jack";
+    char last_name[25];
+
+    printf("Please tell me your name:");
+    scanf("%s", last_name);
+
+    printf("[%s]", name);
+
+    strcat(name, " ");
+    printf("[%s]", name);
+
+}
+
+//What is the difference between a string and a character?
+    //
+
+//What types of quotation marks do we need for a string?
+    //
+
+//What library do we need to include to access the string functions in C?
+    //
+
+//List functions the library allows us to use.
+    //
+
+//How do we concatenate strings in C?
+    //
+
+//How do we get individual characters from a string in C?
+    //

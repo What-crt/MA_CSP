c8cc1a2239098570859045a18caa92d89f6488f1
What-crt <mykel.allan@ucas-edu.net>
2025-10-11 03:16:13 +0000

    finish name_decorator

--- c_programming/projects/name_decorator.c
+++ c_programming/projects/name_decorator.c
@@ -1,17 +1,4 @@
 // MA 6th name decorator for c
-
-#include<stdio.h>
-#include<string.h>
-int main(void){
-
-    char name[30];
-
-    printf("Tell me your name:");
-    scanf(name, sizeof(name), stdin);
-    printf("<-<- %s ->->", name);
-
-    return 0;
-}
 #include <stdio.h>
 #include <string.h>
 

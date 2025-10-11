7b5cd9ebf443d7119945e197ecfd1fde461fdc71
What-crt <mykel.allan@ucas-edu.net>
2025-10-11 03:03:42 +0000

    name_decorator

--- c_programming/projects/name_decorator.c
+++ c_programming/projects/name_decorator.c
@@ -12,3 +12,21 @@ int main(void){
 
     return 0;
 }
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+
+    char name[30];
+    char decorated[100] = "<<< ";
+
+    printf("Tell me your name: ");
+    fgets(name, sizeof(name), stdin);
+    name[strcspn(name, "\n")] = 0;
+
+    strcat(decorated, name);
+    strcat(decorated, " >>>");
+
+    printf("%s\n", decorated);
+    return 0;
+}

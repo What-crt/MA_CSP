6c6891fe832a3f2fab913df238663f8ce2c9ab52
What-crt <mykel.allan@ucas-edu.net>
2025-10-14 04:05:08 +0000

    finished old_enough project

--- c_programming/projects/old_enough.c
+++ c_programming/projects/old_enough.c
@@ -1,7 +1,28 @@
 //MA 6th old enough project for c
 
 #include<stdio.h>
+#include<strings.h>
 int main(void){
-    int
+
+int age;
+    printf("What is your age?: ");
+    scanf("%d", &age);
+
+    if (age >= 18){
+        printf("You are old enough to vote!\n");
+    }
+    else if (age >= 16){
+        printf("You are old enough to drive!\n");
+    }
+    else if (age == 15){
+        printf("You are old enough to get a learners permit!\n");
+    }
+    else if (age >= 4){
+        printf("You are old enough to go to school!\n");
+    }
+    else{
+        printf("You are not old enough to go to school!\n");
+    }
+return 0;
 }
 

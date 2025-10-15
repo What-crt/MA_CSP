ddde7605904ff9a8e82548b78cd8d23bafa81278
What-crt <mykel.allan@ucas-edu.net>
2025-10-15 13:34:23 -0600

    update hello world done!

---
+++ c_programming/projects/update_hello_world.c
@@ -0,0 +1,16 @@
+// MA 6th update hello world for c
+#include <stdio.h>
+void hello(char name[]){
+    printf("Hello %s!\n", name);
+}
+int main (void){
+
+    hello("Sam");
+    hello("Glenn");
+    hello("Eliza");
+    hello("Sarah");
+    hello("Oscar");
+    
+
+    return 0;
+}

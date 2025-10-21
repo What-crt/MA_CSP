3612593f672194a4f74ea089c17f17a6854daa42
What-crt <mykel.allan@ucas-edu.net>
2025-10-21 13:16:52 -0600

    ramdom

---
+++ c_programming/projects/random_number.c
@@ -0,0 +1,17 @@
+// MA 6th random number
+
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main(void){
+    char letters[] = {'a', 'b', 'c', 'd', 'e'};
+    srand(time(NULL));
+    for(int i=0; i< 10; i++){
+    int num = rand() %5;
+    int num = rand() %20 +1;
+    printf("%d\n", num);
+    printf("%c\n", letters[num]);
+    }
+    return 0;
+}
